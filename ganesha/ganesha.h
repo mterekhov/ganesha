960465f27616cd903b77c89a9498024ba52767ed
Michael Terekhov <m.terekhov@icloud.com>
2023-12-20 08:55:21 +0300

    making normal umbrella header

--- ganesha/ganesha.h
+++ ganesha/ganesha.h
@@ -10,6 +10,25 @@
 
 //  This is umbrella header of Ganesha engine
 
+#include "gcolor.h"
+#include "gmatrix.h"
+#include "gpoint.h"
+#include "gpoint2d.h"
+#include "gvector.h"
+#include "glog.h"
+#include "gbundle.h"
+#include "gcolor.h"
+#include "ggraphicsapiprotocol.h"
+#include "gvulkanapi.h"
+#include "gvulkaninstance.h"
+#include "gvulkandevice.h"
+#include "gvulkanpipeline.h"
+#include "gvulkanswapchain.h"
+#include "gvulkancommands.h"
+#include "gvulkanbuffer.h"
+#include "gvulkandescriptorsets.h"
+#include "gcamera.h"
+#include "grendergraph.h"
 #include "gganesha.h"
 #include "ganeshatypes.h"
 
