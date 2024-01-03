cfb061bdc36c93547aa1c2b1d170350674415761
Michael Terekhov <m.terekhov@icloud.com>
2024-01-04 00:36:56 +0300

    triagnle indexes order changed

--- doom/AppDelegate.swift
+++ doom/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         window = createWindow()
-        window?.toggleFullScreen(nil);
+//        window?.toggleFullScreen(nil);
         NSApp.mainMenu = createMenubar()
     }
 
