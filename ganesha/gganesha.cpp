dea2c0d87ae390af9cc5d7995d841fc6c8ec6c8e
Michael Terekhov <m.terekhov@icloud.com>
2024-01-08 02:04:53 +0300

    removing define of vertexes data and it's indexes

--- ganesha/gganesha.cpp
+++ ganesha/gganesha.cpp
@@ -12,32 +12,7 @@ GGanesha::~GGanesha() {
     delete graphicsAPI;
 }
 
-TBool GGanesha::initEngine(void *metalLayer, const TUInt width, const TUInt height) {
-    const std::vector<Vertex> vertexes = {
-        { GPoint(-0.5f, -0.3, -0.5f), GPoint2D{2.0f, 0.0f} },
-        { GPoint(0.5f, -0.3, -0.5f), GPoint2D{0.0f, 0.0f} },
-        { GPoint(0.5f, -0.3, 0.5f), GPoint2D{0.0f, 2.0f} },
-        { GPoint(-0.5f, -0.3, 0.5f), GPoint2D{2.0f, 2.0f} },
-
-        { GPoint(0.0f, -0.1, 0.0f), GPoint2D{2.0f, 0.0f} },
-        { GPoint(1.0f, -0.1, 0.0f), GPoint2D{0.0f, 0.0f} },
-        { GPoint(1.0f, -0.1, 1.0f), GPoint2D{0.0f, 2.0f} },
-        { GPoint(0.0f, -0.1, 1.0f), GPoint2D{2.0f, 2.0f} },
-
-        { GPoint(-2.5f, 0.0f, -2.5f), GPoint2D{2.0f, 0.0f} },
-        { GPoint(2.5f, 0.0f, -2.5f), GPoint2D{0.0f, 0.0f} },
-        { GPoint(2.5f, 0.0f, 2.5f), GPoint2D{0.0f, 2.0f} },
-        { GPoint(-2.5f, 0.0f, 2.5f), GPoint2D{2.0f, 2.0f} }
-    };
-    renderGraph.defineVertexesArray(vertexes);
-
-    const TIndexArray indexes = {
-        2, 1, 0, 0, 3, 2,
-        6, 5, 4, 4, 7, 6,
-        10, 9, 8, 8, 11, 10
-    };
-    renderGraph.defineIndexesArray(indexes);
-    
+TBool GGanesha::initEngine(void *metalLayer, const TUInt width, const TUInt height) {    
     graphicsAPI->initAPI(metalLayer, width, height, renderGraph);
     graphicsAPI->installIsometricView(M_PI_4, 0.000001, 100.0f);
     graphicsAPI->installViewMatrix(camera.viewMatrix());
